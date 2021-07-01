6352e2d4d461a42a507e0dcde285a65c4f8d69ce
uksir <yeah126139163@163.com>
2021-07-01 23:33:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="box">
+    <h1>待办列表</h1>
+    <div class="tool">
+      <input
+        autofocus
+        type="text"
+        v-model="inp"
+        @keyup.enter="add"
+        placeholder="请输入代办事项"
+      >
+    </div>
+    <ul>
+      <li
+        v-for="(item,index) in list"
+        :key="item.id"
+      >
+        <div> <input
+            type="checkbox"
+            v-model="item.checked"
+          >
+          <span :class="item.checked?'finished':''">{{item.value}}</span>
+        </div>
+
+        <button @click="onDelete(index)">X</button>
+      </li>
+    </ul>
+    <section>
+
+      <div><b>{{lefts}}</b> 未完成</div>
+      <a
+        href="javascript:;"
+        @click="clearFinish"
+      >清理已经完成的 <b>{{finished}}</b> 项目 </a>
+    </section>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, computed } from "vue";
+export default {
+  name: "App",
+  setup() {
+    const list = reactive([]);
+    const inp = ref("");
+    const add = () => {
+      list.unshift({ id: Date.now(), value: inp.value, checked: false });
+      inp.value = "";
+      console.log(list);
+    };
+    const clearFinish = () => {
+      const newList = list.filter((v) => !v.checked);
+      list.length = 0;
+      list.push(...newList);
+    };
+    const onDelete = (index) => {
+      list.splice(index, 1);
+    };
+    const lefts = computed(() => list.filter((v) => !v.checked).length);
+    const finished = computed(() => list.filter((v) => v.checked).length);
+    return { list, inp, add, lefts, finished, clearFinish, onDelete };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: #ccc;
+}
+ul {
+  list-style: none;
+}
+li {
+  padding: 20px;
+  text-align: left;
+  font-size: 30px;
+  border-bottom: 1px dashed #ccc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+li input {
+  margin-right: 10px;
+}
+
+li button {
+  display: none;
+  padding: 5px;
+}
+li:hover button {
+  display: inline-block;
+  cursor: pointer;
+}
+.finished {
+  text-decoration: line-through;
+}
+h1 {
+  margin-bottom: 10px;
+}
+.box {
+  background-color: #fff;
+  width: 60vw;
+  padding: 20px;
+  margin: 50px auto;
+  padding-bottom: 0;
+}
+.box .tool input {
+  /* border: none; */
+  width: 100%;
+  height: 50px;
+  text-indent: 20px;
+  font-size: 20px;
+  font-style: italic;
+  color: #666666;
+  font-weight: 700;
+}
+section {
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+a {
+  text-decoration-color: #666;
+  color: inherit;
+}
+</style>
